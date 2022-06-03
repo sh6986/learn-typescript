f275fd025d35e4927d760fb0b154496086f8b900
sh6986 <okok960411@gmail.com>
2022-06-04 00:08:46 +0900

    자바스크립트를 타입스크립트처럼 코딩하는 방법

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -1,6 +1,19 @@
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// sum(10, '20');
+// var result = sum(10, 20);
+
+// @ts-check
+
+/**
+ * 
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
 function sum(a, b) {
     return a + b;
 }
 
 // sum(10, '20');
-// var result = sum(10, 20);
--- why-ts/sample.ts
+++ why-ts/sample.ts
@@ -4,3 +4,5 @@ function add(a: number,  b: number): number {
 
 var result = add(10, 20);
 result.toLocaleString();
+
+// add(10, '20');
