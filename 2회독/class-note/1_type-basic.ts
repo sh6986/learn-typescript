c92fef6ba5ba7bf7d06e7b57ba254061b2f4116f
sh6986 <okok960411@gmail.com>
2022-07-05 23:52:31 +0900

    기본 타입 - 튜플, 객체, 진위값

--- 2회독/class-note/1_type-basic.ts
+++ 2회독/class-note/1_type-basic.ts
@@ -10,4 +10,21 @@ let num: number = 10;
 // TS 배열
 let arr: Array<number> = [1, 2, 3];
 let heroes: Array<string> = ["a", "b", "c", 10];
-let items: number[] = [1, 2, 3];
+let items: number[] = [1, 2, 3, "a"];
+
+// TS 튜플
+let address: [string, number] = ["gangnam", 100];
+
+// TS 객체
+let obj: object = {};
+// let person: object = {
+//   name: "capt",
+//   age: 100,
+// };
+let person: { name: string; age: number } = {
+  name: "thor",
+  age: 1000,
+};
+
+// TS 진위값
+let show: boolean = true;
