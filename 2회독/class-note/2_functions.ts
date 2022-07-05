a4a000fd5ae9a33ac307847fdca5cf0de91f526a
sh6986 <okok960411@gmail.com>
2022-07-06 00:33:56 +0900

    함수 타입 - 옵셔널 파라미터

--- 2회독/class-note/2_functions.ts
+++ 2회독/class-note/2_functions.ts
@@ -15,3 +15,8 @@ function sum(a: number, b: number): number {
   return a + b;
 }
 sum(10, 20, 30, 40);
+
+// 함수의 옵셔널 파라미터
+function log(a: string, b?: string) {}
+log("hello world");
+log("hello ts", "abc");
